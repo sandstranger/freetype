d3725582c5db79b8908421c9f196d8338ed20831
Evgeniy Mineev <e.mineev@crazypandagames.com>
2026-07-13 00:26:29 +0400

    fix compiling

--- builds/unix/ftsystem.c
+++ builds/unix/ftsystem.c
@@ -68,7 +68,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
+#include <fcntl.h>
+#include <unistd.h>
 
   /**************************************************************************
    *
